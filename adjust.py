b8735c9baf5bd176fe3f5662c20674e758af6db4
Josh Nisly <23241028+joshnisly@users.noreply.github.com>
2016-03-25 18:51:13 +0000

    More work on adjustments.

--- adjust.py
+++ adjust.py
@@ -5,21 +5,22 @@ import motor
 
 def adjust():
     motors = [
-        motor.MotorDef(draw._X_MOTOR_PINS, 'x'),
-        motor.MotorDef(draw._Y_MOTOR_PINS, 'y', True)
+        motor.MotorDef(draw._X_MOTOR_PINS, 'x', 1, True),
+        motor.MotorDef(draw._Y_MOTOR_PINS, 'y', 2)
     ]
-    driver = motor.MotorDriver(motors)
+    driver = motor.AdaMotorDriver(motors)
     last = None
+    STEP_SIZE = 1
     while True:
         choice = raw_input('Enter lrupq:') or last
         if choice == 'l':
-            driver.move([motor.MotorMovement('x', True, 0.25)])
+            driver.move([motor.MotorMovement('x', True, STEP_SIZE)])
         elif choice == 'r':
-            driver.move([motor.MotorMovement('x', False, 0.25)])
+            driver.move([motor.MotorMovement('x', False, STEP_SIZE)])
         elif choice == 'u':
-            driver.move([motor.MotorMovement('y', True, 0.25)])
+            driver.move([motor.MotorMovement('y', True, STEP_SIZE)])
         elif choice == 'd':
-            driver.move([motor.MotorMovement('y', False, 0.25)])
+            driver.move([motor.MotorMovement('y', False, STEP_SIZE)])
         elif choice == 'q':
             return
 
