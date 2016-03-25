c8617361d16ca118d25a50d02a664c19a5e014e5
Josh Nisly <23241028+joshnisly@users.noreply.github.com>
2016-03-25 18:54:02 +0000

    Add support for Adafruit hat.

--- motor.py
+++ motor.py
@@ -2,6 +2,7 @@
 
 try:
     import RPi.GPIO as GPIO
+    from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
     FAKE_MOTORS = False
 except ImportError:
     FAKE_MOTORS = True
@@ -9,15 +10,15 @@ except ImportError:
 import threading
 import time
 
-_STEPS_PER_SQUARE = 192
-_SLEEP_INTERVAL = 2 / 1000.0
+_SLEEP_INTERVAL = 2.5 / 1000.0
 
 
 class MotorDef(object):
-    def __init__(self, pins, motor_id, auto_reverse=False):
+    def __init__(self, pins, motor_id, ada_id, auto_reverse=False):
         self.pins = pins
         self.motor_id = motor_id
         self.auto_reverse = auto_reverse
+        self.ada_id = ada_id
 
 
 class MotorMovement(object):
@@ -27,7 +28,45 @@ class MotorMovement(object):
         self.squares = squares
 
 
-class MotorDriver(object):
+class AdaMotorDriver(object):
+    _STEPS_PER_SQUARE = 43
+    def __init__(self, motors):
+        if FAKE_MOTORS:
+            return
+        self._hat = Adafruit_MotorHAT()
+        self._motors = {x.motor_id: x for x in motors}
+        self._motor_objs = {}
+        for motor in motors:
+            self._motor_objs[motor.motor_id] = self._hat.getStepper(200, motor.ada_id)
+            self._motor_objs[motor.motor_id].setSpeed(300)
+
+    def __del__(self):
+        if FAKE_MOTORS:
+            return
+        for motor in self._motors.values():
+            print 'exiting for', motor.ada_id
+            self._hat.getMotor(motor.ada_id).run(Adafruit_MotorHAT.RELEASE)
+
+    def move(self, movements):
+        if FAKE_MOTORS:
+            return
+        threads = [threading.Thread(target=self._move, args=[x]) for x in movements]
+        for thread in threads:
+            thread.daemon = True
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+    def _move(self, movement):
+        motor = self._motor_objs[movement.motor_id]
+        reverse = self._motors[movement.motor_id].auto_reverse != movement.reverse
+        dir = Adafruit_MotorHAT.BACKWARD if reverse else Adafruit_MotorHAT.FORWARD
+        steps = int(movement.squares * self._STEPS_PER_SQUARE)
+        motor.step(steps, dir, Adafruit_MotorHAT.SINGLE)
+
+class GpioMotorDriver(object):
+    _STEPS_PER_SQUARE = 111
     def __init__(self, motors):
         if FAKE_MOTORS:
             return
@@ -60,7 +99,7 @@ class MotorDriver(object):
     def _move(self, movement):
         time.sleep(_SLEEP_INTERVAL)
         pins = self._motors[movement.motor_id].pins
-        steps = int(movement.squares * _STEPS_PER_SQUARE)
+        steps = int(movement.squares * self._STEPS_PER_SQUARE)
         reverse = self._motors[movement.motor_id].auto_reverse != movement.reverse
         movement_sequence = reversed(self._movement_sequence) if reverse else self._movement_sequence
         pin_vals = [zip(pins, x) for x in movement_sequence]
